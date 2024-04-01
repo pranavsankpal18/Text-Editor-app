43a66dbb3a50984062be77684b945d3fbf198bdc
pranavsankpal18 <pranavsankpal18@gmail.com>
2024-04-01 14:53:38 +0530

    routers added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Alert from "./Components/Alert";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
 import React,{useState} from "react";
+import { BrowserRouter as Router,
+Routes,
+Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");   //wether dark mode enable or not
@@ -24,25 +27,34 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode is enabled","success");
+      document.title = 'Text Editor - Dark Mode';
     }
     else{
       setMode("light");
       document.body.style.backgroundColor =  "white";
       showAlert("Light mode is enabled","success");
+      document.title = 'Text Editor - Light Mode';
     }
   }
   return (
-    <>
+       
+      <>
+      <Router> 
       <Navbar title={"TextUtils"} about={"About"} mode={mode} toggleMode={toggleMode}/>
 
       <Alert alert={alert}/>
       
       <div className="container my-3">
-      <TextForm heading="Enter the text analyze below" mode={mode} showAlert={showAlert}/>
-      </div>
-      {/* <About /> */}
+        <Routes>
+          <Route exact path="/About" element={<About />}></Route>
+          <Route exact path="/" element={<TextForm heading="Enter the text analyze below" mode={mode} showAlert={showAlert}/>}></Route>
+        </Routes>
       
+      </div>
+      
+      </Router>
     </>
+    
   );
 }
 
